a6b5cf6babff9aea780360a934dd614df221b904
Hyo Jae Shin <hyojaeshin@Jays-MacBook-Pro.local>
2021-02-18 14:02:43 -0800

    :)

---
+++ ManCus.py
@@ -0,0 +1,40 @@
+
+#ManCus
+#This module have functions for users
+
+class cus():
+	def add_menu():
+		#add new data into the list
+		pass
+
+	def add_order():
+		#add order
+		pass
+
+	def add_modif():
+		#select food options
+		pass
+
+	def edit_order():
+		#edit order data
+		pass	
+
+	def delete_menu():
+		#delete menu from list
+		pass	
+
+	def delete_order():
+		#delete order from list
+		pass	
+
+	def show_order():
+		#present orders from list
+		pass
+
+	def pay_order():
+		#send order data to DB
+		pass
+
+	def report_sale():
+		#call analized report from DB
+		pass
