a7b3b99ad1ce980fd777debf7c40ef7dcc19b14b
Hyo Jae Shin <hyojaeshin@Jays-MacBook-Pro.local>
2021-02-19 00:56:56 -0800

    Easy Functions are done

--- ManCus.py
+++ ManCus.py
@@ -2,30 +2,48 @@
 #ManCus
 #This module have functions for users
 
+import copy
 import os
-import ManDB
-import ManRcpt
+#import ManDB
+import ManClass
 #Global lists
-list_menus = {}
-#talk to ui
+list_items = {}
 list_orders = {}
 
 navigator_symbol = "/" # This will make the program runnable on any unix based enviroument because it has differnet file system
 if os.name == "nt":
     navigator_symbol = "\\" # This will make the program runnable on Windows
 
-def add_menu(name, catagory, ):
+def add_item(name, catagory, price, *discount):
 	#add new data into the list
 	#read and write lists from the file
-	pass
+	item = ManClass.item()
+	item.set_name(name)
+	item.set_catagory(catagory)
+	item.set_price(price)
+	#default should be 1.0
+	if discount:
+		item.set_discount(discount)
 
-def add_order(name, ):
+	list_items[name] = item
+
+def add_order(name):
 	#add order
+	#copy and paste selected item to order list
+	if list_items:
+		list_orders[name] = copy.deepcopy(list_items[name])
+		return True
+	else:
+		print("Options do not exist")
+		return False
+
+def add_modif(name, ):
+	#select food options
 	#read and write the list
 	pass
 
-def add_modif(name):
-	#select food options
+def edit_item(name, ):
+	#edit item data
 	#read and write the list
 	pass
 
@@ -34,34 +52,48 @@ def edit_order():
 	#read and write the list
 	pass	
 
-def delete_menu():
+def delete_menu(name):
 	#delete menu from list
 	#read and write lists from the file?
-	pass	
+	try:
+		del list_items[name]
+		return True
+	except KeyError:
+		print("Option does not exist")
+		return False
 
-def delete_order():
+def delete_order(name):
 	#delete order from list
 	#read and write the list
-	pass	
+	try:
+		del list_orders[name]
+		return True
+	except KeyError:
+		print("Option does not exist")
+		return False
 
-def show_menu():
+def show_item():
 	#present orders from list
 	#read the orders
-	pass
+	if list_items:
+		return list_items
+	else:
+		print("Option is empty")
+		return False
 
 def show_order():
 	#present orders from list
 	#read the orders
-	pass
+	if list_orders:
+		return list_orders
+	else:
+		print("Order is empty")
+		return False
 
 def pay_order():
 	#send order data to DB
-	ManDB()
-	{} = {}
 	pass
 
 def report_sale(date):
 	#call analized report from DB
-	md = ManDB()
-	md.report(date)
 	pass
