d71776a2fb4633e28d7b330f7b5a32b59647a12b
Hyo Jae Shin <hyojaeshin@Jays-MacBook-Pro.local>
2021-02-18 22:38:42 -0800

    :)J

--- ManClass.py
+++ ManClass.py
@@ -1,17 +1,66 @@
 
-#ManRcpt 
+#ManClass
 #This module contains class to create menu
 
-class menu():
+class item():
 	def __init__():
 		self.name = None
 		self.category = None
 		self.price = 0.0
 		self.discount = 1.0
 
-class rcpt():
+	def get_name():
+		return self.name
+
+	def get_catagory():
+		return self.category
+
+	def get_price():
+		return self.price
+
+	def get_discount():
+		return self.discount
+
+	def set_name(self, name):
+		pass
+
+	def set_catagory():
+		pass
+
+	def set_price():
+		pass
+
+	def set_discount():
+		pass
+
+
+class receipt():
 	def __init__():
-		self.name = None
-		self.category = None
-		self.price = 0.0
+		self.customer_name = None
+		self.orders = None
+		self.total = 0.0
 		self.discount = 1.0
+
+	def get_customer_name():
+		pass
+
+	def get_orders():
+		pass
+
+	def get_total():
+		pass
+
+	def get_discount():
+		pass
+
+	def set_customer_name():
+		pass
+
+	def set_orders():
+		pass
+
+	def set_total():
+		pass
+
+	def set_discount():
+		pass
