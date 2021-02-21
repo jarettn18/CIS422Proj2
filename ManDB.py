f657a7798b7fdab55e42c23a16c5b9df35f3f70c
DesastreuX <poom_perat@hotmail.com>
2021-02-21 01:13:59 -0800

    update

--- ManDB.py
+++ ManDB.py
@@ -31,6 +31,17 @@ class ReceiptDatabase:
         self.engine = db.create_engine('sqlite:///ManEzReceipts.sqlite')
         self.connection = self.engine.connect()
         self.metadata = db.MetaData()
+        self.receipts = db.Table('receipts', metadata,
+              db.Column('date', db.date()),
+              db.Column('datetime', db.datetime()),
+              db.Column('name', db.String(255), nullable=False),
+              db.Column('orders', db.String(255), nullable=False),
+              db.Column('discount', db.Float(), default=1.0),
+              db.Column('amount', db.Integer(), default=1),
+              db.Column('price', db.Boolean(), default=True)
+              )
+
+        metadata.create_all(engine) #Creates the table
 
     def add_receipt(self, item):
         pass
