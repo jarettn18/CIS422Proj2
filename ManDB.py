f204949cf3b4089b2fe8b404088ca1b7bed995b7
Hyo Jae Shin <hyojaeshin@Jays-MacBook-Pro.local>
2021-02-21 01:54:29 -0800

    edit

--- ManDB.py
+++ ManDB.py
@@ -13,32 +13,40 @@ class ItemDatabase:
         self.metadata = db.MetaData()
 
         self.item = db.Table('item', metadata,
-                             db.Column('name', db.String(255), nullable=False),
-                             db.Column('category', db.String(255), nullable=False),
-                             db.Column('price', db.Float(), deafault=0.0),
-                             db.Column('discount', db.float(), default=1.0)
-                             )
-
+              db.Column('name', db.String(255), nullable=False),
+              db.Column('category', db.String(255), nullable=False),
+              db.Column('price', db.Float(), deafault=0.0),
+              db.Column('discount', db.float(), default=1.0)
+              )
         metadata.create_all(engine)
 
     def add_item(self, item):
-        if item:
-            query = db.insert(self.item).values(name=item.get_name(), category=item.get_category(),
-                                       price=item.get_price(),
-                                       discount=item.get_discount())
-            ResultProxy = connection.execute(query)
+      if item:
+        query = db.insert(self.item).values(name=item.get_name(), category=item.get_category(),
+                  price=item.get_price(),
+                  discount=item.get_discount())
+        ResultProxy = connection.execute(query)
+      else:
+        print("Invalid Input")
+        return False
 
     def delete_item(self, name):
-        if name:
-            query = db.delete(self.item)
-            query = query.where(self.item.columns.name == item.name)
-            results = connection.execute(query)
+      if name:
+        query = db.delete(self.item)
+        query = query.where(self.item.columns.name == item.name)
+        results = connection.execute(query)
+      else:
+        print("Invalid Input")
+        return False
 
     def edit_item(self, name, option, new_value):
-        if name and option, new_value:
-            query = db.update(self.item).values(name=item.name)
-            query = query.where(self.item.columns.Id == 1)
-            results = connection.execute(query)
+      if name and option, new_value:
+        query = db.update(self.item).values(name=item.name)
+        query = query.where(self.item.columns.Id == 1)
+        results = connection.execute(query)
+      else:
+        print("Invalid Input")
+        return False
 
 class ReceiptDatabase:
     def __init__(self):
