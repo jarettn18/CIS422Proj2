4f32bd73712a66aff4b9ec7a706521f89f55e2e9
avilla2 <a.villa12@outlook.com>
2021-02-22 18:49:50 -0800

    manui updates

--- ManUI.py
+++ ManUI.py
@@ -11,15 +11,29 @@
 """
 import tkinter as tk
 
-class App (tk.frame):
+class App (tk.Frame):
 	
-	def __init__(self, master):
+	def __init__(self, master=None):
 		super().__init__(master)
+		self.master = master
 		self.pack()
+		self.load_test_screen
+
+	def load_test_screen(self):
+		self.make_menu = tk.Button(self)
+		self.make_menu["text"] = "Create New Menu"
+		self.make_menu["command"] = self.make_menu_screen()
+		self.make_menu.pack(side="top")
+		self.quit = tk.Button(self, text="QUIT", fg="red",
+							  command=self.master.destroy)
+		self.quit.pack(side="bottom")
+
+	def make_menu_screen(self):
+		pass
 
 def main():
 	manez = App()
-	myapp.mainloop()
+	manez.mainloop()
 
 if __name__ == '__main__':
 	main()
