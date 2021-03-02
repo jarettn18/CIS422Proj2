a73fba80b6717cae23b14215ea94eabd98e1ad91
jarettn18 <jarettn18@gmail.com>
2021-03-01 21:06:47 -0800

    Init Screen Finish

--- ManUI.py
+++ ManUI.py
@@ -17,43 +17,55 @@ class App (tk.Frame):
 		super().__init__(master)
 		self.master = master
 
-	def _init_submit_button(self, name_var, pin_var):
-		name = name_var.get()
-		pin = pin_var.get()
-
-		# insert name/pin in database
-		print(name)
-		print(pin)
-
-		name_var.set("")
-		pin_var.set("")
-
 	def init_screen(self):
 
+		def _init_submit_button():
+			name = name_var.get()
+			pin = pin_var.get()
+
+			# insert name/pin in database
+			print(name)
+			print(pin)
+
+			name_var.set("")
+			pin_var.set("")
+
+		for i in range(6):
+			tk.Grid.rowconfigure(self.master, i, weight=1)
+			tk.Grid.columnconfigure(self.master, i, weight=1)
+		"""
+		for r in range(6):
+			for c in range(6):
+				tk.Label(self.master, text='R%s/C%s' % (r, c),
+						 borderwidth=1).grid(row=r, column=c)
+		"""
 		#Choose Background Color
 		bg_color = 'gray'
 		self.master.configure(background=bg_color)
 
 		#Create Labels
-		greeting = tk.Label(background=bg_color, text="Welcome to ManEz", font=("Arial", 25))
-		greeting.grid(row=0, column=3, pady=20)
+		greeting = tk.Label(background=bg_color, text="Welcome to ManEz", font=("Arial", 30, 'bold'))
+		greeting.grid(row=0, column=2, columnspan=2)
 		create_profile = tk.Label(background=bg_color, text="Create Administrator Profile", font=("Arial", 25))
-		create_profile.grid(row=2, column=3)
+		create_profile.grid(row=1, column=2, columnspan=2)
 
 		name_var = tk.StringVar()
 		pin_var = tk.StringVar()
 
-		name_label = tk.Label(self.master, text="Name", font=("Calibre", 20, 'bold'))
+		name_label = tk.Label(self.master,background=bg_color, text="Name", font=("Calibre", 20, 'bold'))
 		name_entry = tk.Entry(self.master, textvariable=name_var, font=("Calibre", 20))
 
-		pin_label = tk.Label(self.master, text="Pin", font=("Calibre", 20, 'bold'))
+		pin_label = tk.Label(self.master,background=bg_color, text="Pin", font=("Calibre", 20, 'bold'))
 		pin_entry = tk.Entry(self.master, textvariable=pin_var, font=("Calibre", 20))
 
-		name_label.grid(row=5, column=5)
-		name_entry.grid(row=5, column=6)
+		name_label.grid(row=2, column=2)
+		name_entry.grid(row=2, column=3)
 
-		pin_label.grid(row=6, column=5)
-		pin_entry.grid(row=6, column=6)
+		pin_label.grid(row=3, column=2)
+		pin_entry.grid(row=3, column=3)
+
+		submit = tk.Button(self.master, text="Submit", command=_init_submit_button, font=("Calibre", 20, 'bold'))
+		submit.grid(row=4, column=2)
 
 	def make_menu_screen(self):
 		pass
