b31611f05ce017a951ae9232def88615159b4301
avilla2 <a.villa12@outlook.com>
2021-03-12 03:05:24 -0800

    analysis cleanup

--- ManUI.py
+++ ManUI.py
@@ -15,6 +15,7 @@ import ManCus as cus
 import ManStaff as stf
 import ManReport as rep
 import datetime as dt
+
 BG_COLOR = 'gray'
 
 
@@ -111,7 +112,8 @@ class App(tk.Frame):
 			analysis = ShowSaleData(self.master)
 			analysis.findBySale(tup, tup)
 
-			back = tk.Button(self.master, text="Back", command=lambda: self.main_login_screen(), font=("Calibre", 20, 'bold'))
+			back = tk.Button(self.master, text="Back", command=lambda: self.main_login_screen(),
+							 font=("Calibre", 20, 'bold'))
 			back.grid(row=2, column=1)
 
 			title = tk.Label(background=BG_COLOR, text="Order History", font=("Arial", 50, 'bold'))
@@ -154,10 +156,12 @@ class App(tk.Frame):
 							 font=("Calibre", 30, 'bold'))
 		settings.grid(row=4, column=1)
 
-		clock_in = tk.Button(self.master, text="Clock In", command=lambda: self.pin_screen("clock"), font=("Calibre", 30, 'bold'))
+		clock_in = tk.Button(self.master, text="Clock In", command=lambda: self.pin_screen("clock"),
+							 font=("Calibre", 30, 'bold'))
 		clock_in.grid(row=4, column=4)
 
-		clock_out = tk.Button(self.master, text="Clock Out", command=lambda: self.pin_screen("clock_out"), font=("Calibre", 30, 'bold'))
+		clock_out = tk.Button(self.master, text="Clock Out", command=lambda: self.pin_screen("clock_out"),
+							  font=("Calibre", 30, 'bold'))
 		clock_out.grid(row=5, column=4)
 
 		name_label = tk.Label(self.master, background=BG_COLOR, text="Administrator Use Only",
@@ -175,7 +179,8 @@ class App(tk.Frame):
 				if stf.is_admin(name.lower()) and stf.admin_entry(name.lower(), pin) == 39:
 					self.settings_menu()
 				else:
-					admin_label = tk.Label(self.master, background=BG_COLOR, text="Invalid Login", font=("Calibre", 20, "bold"))
+					admin_label = tk.Label(self.master, background=BG_COLOR, text="Invalid Login",
+										   font=("Calibre", 20, "bold"))
 					admin_label.grid(row=0, column=4)
 			elif mode == "clock":
 				succ = stf.login(name.lower(), pin)
@@ -261,13 +266,16 @@ class App(tk.Frame):
 		button0.grid(row=5, column=4)
 
 		if mode == "clock" or mode == "settings":
-			login_button = tk.Button(self.master, text="login", command=lambda: login(mode), font=("Calibre", 25, 'bold'))
+			login_button = tk.Button(self.master, text="login", command=lambda: login(mode),
+									 font=("Calibre", 25, 'bold'))
 			login_button.grid(row=1, column=4)
 		else:
-			logout_button = tk.Button(self.master, text="logout", command=lambda: login(mode), font=("Calibre", 25, 'bold'))
+			logout_button = tk.Button(self.master, text="logout", command=lambda: login(mode),
+									  font=("Calibre", 25, 'bold'))
 			logout_button.grid(row=1, column=4)
 
-		back = tk.Button(self.master, text="Back", command=lambda: self.main_login_screen(), font=("Calibre", 20, 'bold'))
+		back = tk.Button(self.master, text="Back", command=lambda: self.main_login_screen(),
+						 font=("Calibre", 20, 'bold'))
 		back.grid(row=1, column=1)
 
 	def settings_menu(self):
@@ -286,7 +294,8 @@ class App(tk.Frame):
 								text='Create New Account', width='25', height='10', font=("Calibre", 20, 'bold'))
 		new_account.grid(row=2, column=1)
 
-		sales = tk.Button(settings_frame, cursor='circle', command=lambda: self.analysis() ,text='Sales Analytics', width='25', height='10',
+		sales = tk.Button(settings_frame, cursor='circle', command=lambda: self.analysis(), text='Sales Analytics',
+						  width='25', height='10',
 						  font=("Calibre", 20, 'bold'))
 		sales.grid(row=2, column=3)
 
@@ -295,7 +304,8 @@ class App(tk.Frame):
 						 font=("Calibre", 20, 'bold'))
 		edit.grid(row=3, column=3)
 
-		employee = tk.Button(settings_frame, cursor='circle', command=lambda: self.emp_analysis(), text='Employee Analytics', width='25', height='10',
+		employee = tk.Button(settings_frame, cursor='circle', command=lambda: self.emp_analysis(),
+							 text='Employee Analytics', width='25', height='10',
 							 font=("Calibre", 20, 'bold'))
 		employee.grid(row=3, column=1)
 
@@ -418,7 +428,8 @@ class App(tk.Frame):
 		title = tk.Label(title_frame, text='New Order', font=("Calibre", 20, 'bold'), width='15', height='5')
 		title.grid(row=1, column=2)
 
-		back = tk.Button(title_frame, command=lambda: self.main_login_screen(), text="Back", font=("Calibre", 20, 'bold'))
+		back = tk.Button(title_frame, command=lambda: self.main_login_screen(), text="Back",
+						 font=("Calibre", 20, 'bold'))
 		back.grid(row=1, column=1)
 
 		action_frame = tk.Frame(order_main_frame)
@@ -433,7 +444,7 @@ class App(tk.Frame):
 		self.reset()
 
 		title_frame = tk.Frame(self.master, background=BG_COLOR)
-		title_frame.grid(row=1, column=3)
+		title_frame.grid(row=1, column=2, columnspan=3)
 
 		back = tk.Button(title_frame, command=lambda: self.settings_menu(), text="Back", font=("Calibre", 20, 'bold'))
 		back.pack(side=tk.LEFT)
@@ -447,7 +458,7 @@ class App(tk.Frame):
 		date.grid(row=2, column=1)
 
 		to = tk.Label(self.master, text='To:', background=BG_COLOR, font=("Calibre", 16, 'bold'), width='10',
-						height='5')
+					  height='5')
 		to.grid(row=2, column=3)
 
 		from_frame = tk.Frame(self.master)
@@ -468,65 +479,71 @@ class App(tk.Frame):
 		month = ttk.Combobox(from_frame, state="readonly", style="bg.TCombobox", textvariable=monthvar,
 							 font=("Calibre", 16, 'bold'), width='10', height='5')
 		month['values'] = (
-		'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November',
-		'December')
+			'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+			'November',
+			'December')
 		month.pack(side=tk.LEFT)
-		month.current(int(month_today)-1)
+		month.current(int(month_today) - 1)
 
 		dayvar = tk.IntVar()
 		day = ttk.Combobox(from_frame, state="readonly", style="bg.TCombobox", textvariable=dayvar,
 						   font=("Calibre", 16, 'bold'), width='5', height='5')
 		day['values'] = [x for x in range(1, 32)]
 		day.pack(side=tk.LEFT)
-		day.current(int(day_today)-1)
+		day.current(int(day_today) - 1)
 
 		yearvar = tk.IntVar()
 		year = ttk.Combobox(from_frame, state="readonly", style="bg.TCombobox", textvariable=yearvar,
-						   font=("Calibre", 16, 'bold'), width='5', height='5')
+							font=("Calibre", 16, 'bold'), width='5', height='5')
 		year['values'] = (2021)
 		year.pack(side=tk.LEFT)
 		yearvar.set(int(year_today))
 
 		tomonthvar = tk.StringVar()
 		tomonth = ttk.Combobox(to_frame, state="readonly", style="bg.TCombobox", textvariable=tomonthvar,
-							 font=("Calibre", 16, 'bold'), width='10', height='5')
+							   font=("Calibre", 16, 'bold'), width='10', height='5')
 		tomonth['values'] = (
 			'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
 			'November',
 			'December')
 		tomonth.pack(side=tk.LEFT)
-		tomonth.current(int(month_today)-1)
+		tomonth.current(int(month_today) - 1)
 
 		todayvar = tk.IntVar()
 		today = ttk.Combobox(to_frame, state="readonly", style="bg.TCombobox", textvariable=todayvar,
-						   font=("Calibre", 16, 'bold'), width='5', height='5')
+							 font=("Calibre", 16, 'bold'), width='5', height='5')
 		today['values'] = [x for x in range(1, 32)]
 		today.pack(side=tk.LEFT)
-		today.current(int(day_today)-1)
+		today.current(int(day_today) - 1)
 
 		toyearvar = tk.IntVar()
 		toyear = ttk.Combobox(to_frame, state="readonly", style="bg.TCombobox", textvariable=toyearvar,
-							font=("Calibre", 16, 'bold'), width='5', height='5')
+							  font=("Calibre", 16, 'bold'), width='5', height='5')
 		toyear['values'] = (2021)
 		toyear.pack(side=tk.LEFT)
 		toyearvar.set(int(year_today))
 
-		button_frame = tk.Frame(self.master, background=BG_COLOR,)
+		button_frame = tk.Frame(self.master, background=BG_COLOR, )
 		button_frame.grid(row=2, column=5)
 
 		saleData = ShowSaleData(self.master)
 
 		findBy = tk.Label(button_frame, text='Find By:', background=BG_COLOR, font=("Calibre", 16, 'bold'),
-						 width='20')
+						  width='20')
 		findBy.pack(side=tk.TOP)
 
-		findBySale = tk.Button(button_frame, background=BG_COLOR, command=lambda: saleData.findBySale((yearvar.get(), monthvar.get(), dayvar.get()), (toyearvar.get(), tomonthvar.get(), todayvar.get())), text="Sales", font=("Calibre", 16, 'bold'))
+		findBySale = tk.Button(button_frame, width="10", background=BG_COLOR,
+							   command=lambda: saleData.findBySale((yearvar.get(), monthvar.get(), dayvar.get()),
+																   (toyearvar.get(), tomonthvar.get(), todayvar.get())),
+							   text="Sales", font=("Calibre", 16, 'bold'))
 		findBySale.pack(side=tk.TOP)
 
-		findByItem = tk.Button(button_frame, background=BG_COLOR, command=lambda: self.settings_menu(), text="Items", font=("Calibre", 16, 'bold'))
+		findByItem = tk.Button(button_frame, width="10", background=BG_COLOR, command=lambda: self.settings_menu(), text="Items",
+							   font=("Calibre", 16, 'bold'))
 		findByItem.pack(side=tk.TOP)
 
-		findByCat = tk.Button(button_frame, background=BG_COLOR, command=lambda: self.settings_menu(), text="Categories", font=("Calibre", 16, 'bold'))
+		findByCat = tk.Button(button_frame, width="10", background=BG_COLOR, command=lambda: self.settings_menu(),
+							  text="Categories", font=("Calibre", 16, 'bold'))
 		findByCat.pack(side=tk.TOP)
 
 	def emp_analysis(self):
@@ -544,45 +561,93 @@ class ShowSaleData(tk.Frame):
 	def __init__(self, master=None):
 		super().__init__(master)
 		self.master = master
-		self.months = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6, 'July': 7, 'August': 8, 'September': 9, 'October': 10,'November': 11,'December': 12}
+		self.months = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6, 'July': 7, 'August': 8,
+					   'September': 9, 'October': 10, 'November': 11, 'December': 12}
 		self.ticket = tk.Text(self.master, font=("Calibre", 18, 'bold'))
 		ys = tk.Scrollbar(self.master, orient='vertical', command=self.ticket.yview)
 		self.ticket['yscrollcommand'] = ys.set
-		self.ticket.grid(column=1, row=3, columnspan=6, rowspan=3)
+		self.ticket.grid(column=1, row=4, columnspan=6, rowspan=3)
 		self.ticket['state'] = 'disabled'
+		self.empty = True
+		self.title = title = tk.Label(self.master, text='Select a Date Range', background=BG_COLOR, font=("Calibre", 20, 'bold'))
+		title.grid(row=3, column=1, columnspan=6)
+
+	def _calcTotalPeriod(self, day_totals: list):
+		total_sales = 0
+		total_rev = 0
+		print(day_totals)
+		for i in day_totals:
+			total_sales += i[1]
+			total_rev += i[0]
+		return total_rev, total_sales
+
+	def _calcTotalDay(self, receipts: dict):
+		total = 0
+		for item in receipts:
+			for entry in receipts[item]:
+				total += entry[0] * entry[2] * entry[3]
+		return total
+
+	def _calcTotal(self, collection: list):
+		''' list[ ( amount, name, price, discount ), ... ] '''
+		total = 0
+		for item in collection:
+			total += item[0] * item[2] * item [3]
+		return total
 
 	def findBySale(self, startdate: tuple, enddate: tuple):
+		self.ticket['state'] = 'normal'
+		self.ticket.delete('1.0', tk.END)
 		start = dt.date(startdate[0], self.months[startdate[1]], startdate[2])
 		end = dt.date(enddate[0], self.months[enddate[1]], enddate[2])
 		sales = rep.get_sale_list(start, end)
-		self.ticket['state'] = 'normal'
+		total_sales = rep.total_sale_by_date(start, end)
+		print(rep.report_by_category(start, end))
+		print(rep.report_by_item(start, end))
+		period_totals = []
 		for entry in sales:
 			if sales[entry]:
+				self.empty = False
 				receipt_collection = {}
 				receipt_info = {}
 				for item in sales[entry]:
-					if item[0] in receipt_collection:
-						receipt_collection[item[0]].append((item[6], item[4], item[7], item[5])) # amount, item name, price, discount
+					receipt_num = f"{item[1].year}{item[1].month}{item[1].day}{item[0]}"
+					if receipt_num in receipt_collection:
+						receipt_collection[receipt_num].append(
+							(item[6], item[4], item[7], item[5]))  # amount, item name, price, discount
 					else:
-						receipt_collection[item[0]] = []
-						receipt_collection[item[0]].append((item[6], item[4], item[7], item[5]))
+						receipt_collection[receipt_num] = []
+						receipt_collection[receipt_num].append((item[6], item[4], item[7], item[5]))
 						hr = item[2].strftime("%H")
 						if int(hr) > 12:
 							ampm = "PM"
 						else:
 							ampm = "AM"
 						time = item[2].strftime("%I:%M:%S") + f" {ampm}"
-						receipt_info[item[0]] = (item[3], item[1], time) # Customer Name, Date, Time
+						receipt_info[receipt_num] = (item[3], item[1], time)  # Customer Name, Date, Time
+				day_sales = total_sales[entry]
+				day_total = self._calcTotalDay(receipt_collection)
+				period_totals.append((day_total, day_sales))
+				self.ticket.insert(tk.END, f"-Showing All Sales On {entry} \t\t Day Total: ${day_total} From {day_sales} Sales- \n\n")
 				for item in receipt_collection:
-					self.ticket.insert(tk.END, f"Receipt#: {item} \t Customer: {receipt_info[item][0]}\n")
-					self.ticket.insert(tk.END, f"Date: {receipt_info[item][1]} \t Time: {receipt_info[item][2]}\n ")
-					self.ticket.insert(tk.END, "___________________________________\n")
-					for food in receipt_collection[item]: #order of food: amout, item name, price, discount
-						self.ticket.insert(tk.END, f"{food[0]} \t {food[1]} \t\t ${food[2] * food[3]}\n")
+					self.ticket.insert(tk.END, f"\t\tReceipt#: {item} \t Customer: {receipt_info[item][0]}\n")
+					self.ticket.insert(tk.END, f"\t\tDate: {receipt_info[item][1]} \t Time: {receipt_info[item][2]}\n ")
+					self.ticket.insert(tk.END, "\t\t________________________________________\n")
+					for food in receipt_collection[item]:  # order of food: amount, item name, price, discount
+						self.ticket.insert(tk.END, f"\t\t{food[0]} \t {food[1]} \t\t ${food[0] * food[2] * food[3]}\n")
 						if int(food[3]) != 1:
-							self.ticket.insert(tk.END, f"Price Adjust by %{food[3]}\n")
+							self.ticket.insert(tk.END, f"\t\t\t    Price Adjust by %{food[3]}\n")
+					self.ticket.insert(tk.END, "\t\t\t_______________________________\n")
+					self.ticket.insert(tk.END, f"\t\t\t Total: \t\t ${self._calcTotal(receipt_collection[item])}\n")
 					self.ticket.insert(tk.END, "\n\n")
+		if self.empty:
+			self.title['text'] = "No Orders To Show In Date Range"
+		else:
+			all_totals = self._calcTotalPeriod(period_totals)
+			self.title[
+				'text'] = f"Showing Sales From {start} to {end} \t Period Total: ${all_totals[0]} from {all_totals[1]} Sales"
 		self.ticket['state'] = 'disabled'
+
 
 
 class UpdatingCategories(tk.Frame):
